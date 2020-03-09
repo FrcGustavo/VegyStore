5555e7f23358edcdc22b23194db16ca42644554f
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-08 19:35:10 -0600

    Estucturas de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'store';
+  title = 'VegyStore';
+
+  items = ['nicolas', 'julian', 'perez'];
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem() {
+    this.items.push(this.title);
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
