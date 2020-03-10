d6058c0edc8f90de42949cc06e195929642e461c
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-09 20:28:54 -0600

    creando un shared module y core module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule {
 
-};
+}
