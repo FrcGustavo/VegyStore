b93638a5f214ab7d3e6f46e522790b9c81bb99f2
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-09 18:40:40 -0600

    Creando detalle de cada producto

---
+++ src/app/products.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/brote.jpg',
+      title: 'Brote de albahcar',
+      price: 15,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  constructor() { }
+
+  getAllProducts(): Product[] {
+    return this.products;
+  }
+
+  getProduct(id: string): Product {
+    return this.products.find(item => id === item.id);
+  }
+}
