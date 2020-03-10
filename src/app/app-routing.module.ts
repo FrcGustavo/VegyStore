9157c2dbf9289947e6d641bc20c67167c3e6656a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-09 20:38:43 -0600

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 
+import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
   {
@@ -18,7 +19,7 @@ const routes: Routes = [
       { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'products', component: ProductsComponent },
       { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'contact', component: ContactComponent },
+      { path: 'contact', canActivate: [AdminGuard], component: ContactComponent },
       { path: 'demo', component: DemoComponent },
       { path: '**', component: PageNotFoundComponent }
     ]
