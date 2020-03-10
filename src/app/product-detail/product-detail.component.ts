fdcffd4a3112c9e785e1b98167977df1c8738727
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-09 18:48:53 -0600

    Elaboracion de la paguina de detalle de producto

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '../products.service';
+import { Product } from '../product.model';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+
+  product: Product;
 
   constructor(
     private route: ActivatedRoute,
@@ -16,8 +20,7 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      const product = this.productsService.getProduct(id);
-      console.log(product);
+      this.product = this.productsService.getProduct(id);
     });
   }
 
